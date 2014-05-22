33641e556f576dfd7e509c30c7eaeed8a9d72d87
olewhalehunter <andersenpuckett@gmail.com>
2014-05-21 20:56:19 -0400

    server init

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-NARODNIK
-=======
-народник
------------
+# narodnik-server
 
-Disributed user agent network.
+FIXME
 
-Licensed under the GNU Affero Public License.
+## Prerequisites
 
+You will need [Leiningen][1] 1.7.0 or above installed.
+
+[1]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+## License
+
+Copyright © 2014 FIXME
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject app "1.0.0-SNAPSHOT"
-  :description "Narodnik: Distributed user agent network."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                [org.clojure/clojurescript "0.0-2202"]
-                [org.clojure/java.jdbc "0.3.2"]
-                [http-kit "2.1.16"]
-                [postgresql "9.1-901.jdbc4"]]
-  :main app.core
-)
-
+(defproject narodnik-server "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler narodnik-server.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
