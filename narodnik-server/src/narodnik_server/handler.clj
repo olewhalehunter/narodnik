d38a65cd770d1f798c5491ec89267b2ee90d49e8
olewhalehunter <andersenpuckett@gmail.com>
2014-06-19 15:13:35 -0400

    db init

--- narodnik-server/src/narodnik_server/handler.clj
+++ narodnik-server/src/narodnik_server/handler.clj
@@ -3,33 +3,35 @@
    [aleph.udp]
    [gloss core io]
    [lamina core api]
-   [clojure java jdbc]
-  :require 
-  [clojure.java.jdbc :as sql]))
+))
 
-(let [db-host "localhost"
-      db-port 5432
-      db-name "narodnik"]
- 
-  (def db {:classname "org.postgresql.Driver"
-           :subprotocol "postgresql"
-           :subname (str "//" db-host ":" db-port "/" db-name)
-           :user "postgre")))
+(require '[clojure.java.jdbc :as sql])
 
 (defn init-db []
-  (sql/create-table :machines
-                    [:location "varchar(50)"]
-                    [:id "bigint"]))
+  (let [db-host "localhost"
+        db-port 5432
+        db-name "narodnik"]
+    
+    (def db {
+             :subprotocol "postgresql"
+             :subname (str "//" db-host ":" db-port "/" db-name)
+             :user "postgres"
+             :password "password"}))
 
-(def task-assign-interval 1000)
+  (sql/db-do-commands 
+   db 
+   (sql/create-table-ddl :machines 
+                         [:name :text]
+                         [:id :bigint])))
+
 
 (defn handler-thread []
   "For updating status updates 
    and CRUD operations from slaves"
 
-  (let [inbound-channel @(udp-object-socket 
-                          {:port 10201})
+  (let [inbound-channel @(udp-object-socket {:port 10201})
         timeout 4000]
+
       (Thread/sleep timeout)
       (println "Starting inbound...")
       (eval (read-string
@@ -38,28 +40,36 @@
       (close inbound-channel))
   (handler-thread))
 
+
 (defn task-assign-thread []
   "testing handler thread"
 
-  (Thread/sleep task-assign-interval)
-  (let [outbound-channel @(udp-object-socket)
-        inbound-channel @(udp-object-socket {:port 10202})
+  (let [task-handler-interval 1000
+        inbound-port 10202
+        outbound-port 10201
+        host "localhost"
         text-msg "(println \"NARODNIK IS GO\")"]
-    (try
-      (enqueue outbound-channel 
-               {:message text-msg 
-                :host "localhost" 
-                :port 10201})
-       (finally
-        (close outbound-channel)
-        (close inbound-channel))))
-  (task-assign-thread))
+
+    (Thread/sleep task-handler-interval)
+
+    (let [outbound-channel @(udp-object-socket)
+          inbound-channel @(udp-object-socket {:port inbound-port})]
+      (try
+        (enqueue outbound-channel 
+                 {:message text-msg 
+                  :host host 
+                  :port outbound-port})
+        (finally
+          (close outbound-channel)
+          (close inbound-channel))))
+    (task-assign-thread)))
+
 
 (defn -main [& args]
-  (sql/with-connection db
-    (init-db))
+  (init-db)
   (.start (Thread. task-assign-thread)) 
   (.start (Thread. handler-thread))
 
   (println "NARODNIK SERVER!")
 )
+
